a99e4c682801c0d773f22255bce7452e3358f552
Eric Yang <ericyy87@gmail.com>
2022-10-29 21:56:25 -0400

    2.0.0

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 /* globals hexo: true */
 
 var assign = require('object-assign');
-var filter = require('./lib/filter');
-var slugify = require('./lib/slugify');
+var toc = require('markdown-toc');
 
 var config = hexo.config.toc || {};
-hexo.config.toc = assign({}, config, { slugify: slugify.load(config.slugify) });
+hexo.config.toc = assign({}, config, {
+  slugify: function(name) {
+    var slugify;
+    if (name && typeof name === 'string') {
+      switch (name) {
+        case 'transliteration':
+          slugify = require('transliteration').slugify;
+          break;
+        default:
+          slugify = require('uslug');
+          break;
+      }
+    } else {
+      slugify = require('uslug');
+    }
+    return slugify;
+  }(config.slugify)
+});
 
-hexo.extend.filter.register('before_post_render', filter.insert);
-hexo.extend.filter.register('after_post_render', filter.heading);
+hexo.extend.filter.register('before_post_render', function(data) {
+  var options = assign({}, this.config.toc);
+
+  // add class option
+  if (options.class) {
+    data.content = data.content.replace("<!-- toc -->", '<div class="' + options.class + 'start"></div><!-- toc --><div class="' + options.class + 'end"></div>');
+  }
+
+  data.content = toc.insert(data.content, options);
+  return data;
+});
